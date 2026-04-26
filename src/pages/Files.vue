3ad90e5d11c4095e023bb5d2f5c343c9d5c3f0b8
Joery <44531907+Joery-M@users.noreply.github.com>
2026-04-26 15:34:34 +0200

    wip: remove hash test for now

--- src/pages/Files.vue
+++ src/pages/Files.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
 import FileListView from "@/components/FileListView.vue";
 import FileViewer from "@/components/viewers/FileViewer.vue";
-import { doHashTest } from "@/lib/up2k/test";
-import { useFileDialog, useLocalStorage } from "@vueuse/core";
+import { useLocalStorage } from "@vueuse/core";
 import TreeView from "../components/TreeView.vue";
 
 const fileListType = useLocalStorage<"list" | "grid">("list-type", "list");
-
-const fileDialog = useFileDialog();
-fileDialog.onChange((fileList) => {
-    if (fileList && fileList.length > 0) {
-        doHashTest(fileList.item(0)!);
-    }
-});
 </script>
 
 <template>
@@ -21,7 +13,6 @@ fileDialog.onChange((fileList) => {
             <div class="my-12 mx-6">
                 <FileListView v-if="fileListType === 'list'" />
             </div>
-            <button @click="fileDialog.open()">TEst</button>
         </TreeView>
     </main>
     <FileViewer />
