19cc8efbbf60a4a2ef715623ee62980bee3f1e15
Joery <44531907+Joery-M@users.noreply.github.com>
2026-05-05 14:32:23 +0200

    feat: fetch progress in plain text viewer

--- src/components/viewers/PlainTextEditor.vue
+++ src/components/viewers/PlainTextEditor.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
 import { getApiUrl } from '@/lib/api';
 import { type File } from '@/lib/interop';
+import { fetchWithProgress } from '@/lib/utils';
 import { useQuery } from '@pinia/colada';
+import { useTimeoutFn, whenever } from '@vueuse/core';
 import { effect, ref } from 'vue';
 
 const props = defineProps<{ file: File }>();
 
+const showProgress = ref(true);
+const loadProgress = ref(0);
+
+whenever(
+    () => loadProgress.value === 1,
+    () => useTimeoutFn(() => (showProgress.value = false), 200)
+);
+// If we are still loading after a second, show the progress
+useTimeoutFn(() => {
+    if (loadProgress.value < 1 && !sourceText.value) showProgress.value = true;
+}, 1000);
+
 const fileData = useQuery({
     key: () => ['file', ...(props.file.fullPath ?? [])],
-    query: ({ signal }) => fetch(getApiUrl(props.file.fullPath), { signal }).then((r) => r.text())
+    query: ({ signal }) =>
+        fetchWithProgress(
+            (p) => (loadProgress.value = Math.min(p.transferred / p.size, 1)),
+            getApiUrl(props.file.fullPath),
+            { signal }
+        ).then((r) => r.text()),
+    gcTime: 0,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false
 });
 
 const sourceText = ref('');
@@ -21,6 +43,10 @@ effect(() => {
 </script>
 
 <template>
+    <div
+        class="absolute top-0 left-0 transition-all h-0.5 bg-ring"
+        :style="{ width: loadProgress * 100 + '%', opacity: showProgress ? 1 : 0 }"
+    />
     <textarea v-model="sourceText" class="outline-accent outline px-4" />
 </template>
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -31,3 +31,35 @@ export function formatFileSize(b: number, unit: 'IEC' | 'SI' = 'IEC', useBits = 
     });
     return `${num} ${sizes[i]}`;
 }
+
+export interface FetchProgress {
+    transferred: number;
+    size: number;
+}
+export async function fetchWithProgress(
+    onProgress: (progress: FetchProgress) => void,
+    input: string | URL | Request,
+    init?: RequestInit
+) {
+    const req = await fetch(input, init);
+    const size = parseInt(req.headers.get('Content-Length') ?? '0') ?? 0;
+    let transferred = 0;
+    const reader = req.body!.getReader();
+    const stream = new ReadableStream({
+        async start(controller) {
+            let chunk;
+            while (((chunk = await reader.read()), !chunk.done)) {
+                transferred += chunk.value.byteLength;
+                onProgress({ size, transferred });
+                controller.enqueue(chunk.value);
+            }
+            onProgress({ size, transferred });
+            controller.close();
+        }
+    });
+    return new Response(stream, {
+        headers: req.headers,
+        status: req.status,
+        statusText: req.statusText
+    });
+}
