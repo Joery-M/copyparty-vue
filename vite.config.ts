52fe8a25ad9d06a7dc1f430ab120215ef0a833f8
Joery <44531907+Joery-M@users.noreply.github.com>
2026-05-27 10:04:00 +0200

    wip: enable jinja injection in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import tailwind from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, type Plugin } from 'vite';
 import VueDevtools from 'vite-plugin-vue-devtools';
 
 export default defineConfig((env) => ({
@@ -9,7 +9,8 @@ export default defineConfig((env) => ({
         VueDevtools(),
         VueI18nPlugin({ include: './src/localization/**/*.json' }),
         tailwind(),
-        vue()
+        vue(),
+        customJinjaElementsInjector(process.env.FORCE_JINJA === 'true')
     ],
     resolve: {
         tsconfigPaths: true
@@ -28,3 +29,42 @@ export default defineConfig((env) => ({
         }
     }
 }));
+
+/**
+ * Custom plugin to inject jinja templates into the html during build
+ */
+function customJinjaElementsInjector(force: boolean) {
+    const injectionTemplates: Record<string, string> = {
+        'initial-state': `
+        {%- if og_fn %}
+        <script type="module">
+            window.__initial_state = {
+                dir: \`{{ this.vpath }}\`,
+                preview: \`{{ og_fn }}\`,
+            };
+        </script>
+        {%- endif %}`,
+        'html-head': `{{ html_head }}`
+    };
+    let isDev = false;
+    return {
+        name: 'custom-jinja-elements-injector',
+        configResolved(config) {
+            isDev = config.command === 'serve';
+        },
+        transformIndexHtml(html) {
+            if (isDev && !force) return;
+
+            const injectionKey = /<!-- INJECT: ([A-z0-9-/]+) -->/g;
+            let section;
+            while ((section = injectionKey.exec(html))) {
+                const template = section[1];
+                html = html.replace(section[0], injectionTemplates[template] ?? '');
+            }
+            return {
+                html,
+                tags: []
+            };
+        }
+    } satisfies Plugin;
+}
